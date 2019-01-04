bc34407c7389d62a2e583a70ee3bd266fed24184
Susruth  Nadimpalli <susruth@republicprotocol.com>
2019-01-04 15:42:32 +1100

    scripts: fixed make script

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+mkdir -p ../target/darknode/bin
 
-cd cmd
+cd ../cmd
 xgo -go 1.10 --targets=darwin/amd64 .
 xgo -go 1.10 --targets=linux/amd64 .
-mv cmd-linux-amd64 ../target/bin/darknode_linux_amd64
-mv cmd-darwin-10.6-amd64 ../target/bin/darknode_darwin_amd64
+mv cmd-linux-amd64 ../target/darknode/bin/darknode_linux_amd64
+mv cmd-darwin-10.6-amd64 ../target/darknode/bin/darknode_darwin_amd64
