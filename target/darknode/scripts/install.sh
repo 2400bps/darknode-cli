22a4466c73df76e3204bd603d612e32f1eb0173c
Susruth  Nadimpalli <susruth@republicprotocol.com>
2019-01-04 15:13:33 +1100

    target/darknode/scripts: updated to get releases from darknode-cli repo

--- target/darknode/scripts/install.sh
+++ target/darknode/scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 get_latest_release() {
-  curl -s https://api.github.com/repos/republicprotocol/republic-go/releases/latest \
+  curl -s https://api.github.com/repos/republicprotocol/darknode-cli/releases/latest \
     | grep "browser_download_url.*darknode-$1.zip" \
     | cut -d : -f 2,3 \
     | tr -d \" \
